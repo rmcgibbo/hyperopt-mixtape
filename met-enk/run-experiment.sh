eecd56cfaf9ceca151d0346387afdfa7d0c76541
Robert McGibbon <rmcgibbo@gmail.com>
2014-09-11 16:52:55 -0700

    use zsh in run-experiment.

--- met-enk/run-experiment.sh
+++ met-enk/run-experiment.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-#PBS -S /bin/bash
+#!/bin/zsh
+#PBS -S /bin/zsh
 #PBS -l nodes=1:ppn=16
+#PBS -o /dev/null
+#PBS -e /dev/null
 #PBS -l walltime=24:00:00
 #PBS -V
 #PBS -q MP
 cd $PBS_O_WORKDIR
 
-
 make startserver >| server.log 2>&1 &
 sleep 10
 mpirun -np 15 make runworker >| worker.log 2>&1
+wait
